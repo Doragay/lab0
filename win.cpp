c3b15289aad3dc934a918f86029c98cfd72da30d
Daria <nik-fa01@yandex.ru>
2024-04-07 20:20:52 +0300

    Добавила исходный код,добавила библиотеки,новая связь сигналов и слотов

--- win.cpp
+++ win.cpp
@@ -1,8 +1,34 @@
+#include <win.h>
+#include <QHBoxLayout>
 
-#include "win.h"
 
-win::win()
+win::win(QWidget *parent):QWidget(parent)// Конструктор создает и компанует виджеты
 {
-    print(njvndjk);
+this->setWindowTitle("Счетчик"); //заголовок окна
+label1 = new QLabel("Cчет по 1",this); // метка для сетчика edit1
+label2 = new QLabel("Cчет по 5",this);
+edit1 = new Counter("0",this);//счетчик
+edit2 = new Counter("0",this);
+calcButton=new QPushButton("+1",this); //кнопка
+exitButton=new QPushButton("Выход",this);
+
+QHBoxLayout *layout1 = new QHBoxLayout(); //горизонтальный компановщик
+layout1->addWidget(label1);
+layout1->addWidget(label2);
+QHBoxLayout *layout2 = new QHBoxLayout();
+layout2->addWidget(edit1);
+layout2->addWidget(edit2);
+QHBoxLayout *layout3 = new QHBoxLayout();
+layout3->addWidget(calcButton);
+layout3->addWidget(exitButton);
+QVBoxLayout *layout4 = new QVBoxLayout(this); //вертикальный компановщик
+layout4->addLayout(layout1);
+layout4->addLayout(layout2);
+layout4->addLayout(layout3);
+
+// связь сигнала нажатия кнопки и слота закрытия окна
+connect(calcButton,&QPushButton::clicked, edit1,&Counter::add_one);
+connect(edit1,&Counter::tick_signal,edit2,&Counter::add_one);
+connect(exitButton,&QPushButton::clicked,this,&win::close);
 }
 
--- win.h
+++ win.h
@@ -1,14 +1,41 @@
+#ifndef win_h
+#define win_h
+#include <QtGui>
+#include <QWidget>
+#include <QString>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
 
-#ifndef WIN_H
-#define WIN_H
-
-
-
-
-class win
+class Counter:public QLineEdit
 {
+    Q_OBJECT
 public:
-    win();
-};
+    Counter(const QString &contents, QWidget *parent=0):
+        QLineEdit(contents,parent){}//конструктор
+signals:
+    void tick_signal();
+public slots:
+    void add_one()
+    {
+        QString str=text();
+        int r=str.toInt();// преобразуем строку в целое число
+        r++;
+        if (r!=0 && r%5 ==0) emit tick_signal(); //если значение кратно 5 вызвается tick_signal
 
-#endif // WIN_H
+        str.setNum(r); //преобразуем число в строку
+        setText(str); //устанавливаем текст в строку
+    }
+};
+class win: public QWidget
+{
+    Q_OBJECT
+public:
+    win(QWidget *parent = 0);
+protected:
+    QLabel *label1,*label2;
+    Counter *edit1,*edit2;
+    QPushButton *calcButton;
+    QPushButton *exitButton;
+};
+#endif
